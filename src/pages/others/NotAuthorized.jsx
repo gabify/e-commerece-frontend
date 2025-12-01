d8adf3cc6ec77a7cbafeed29a0988dac3e2c35bb
gabify <ivangabriel.talaoc@g.batstate-u.edu.ph>
2025-12-02 06:30:30 +0800

    Added  Not found page, error page, not authorized page and loading component

--- src/pages/others/NotAuthorized.jsx
+++ src/pages/others/NotAuthorized.jsx
@@ -1,7 +1,29 @@
+import Header from "../../components/Header";
+import AccessDenied from "../../assets/access_denied.svg";
+
 const NotAuthorized = () =>{
     return(
         <>
-           
+          <Header />
+
+        <section  className="p-10 font-Quicksand grid grid-cols-1 justify-items-center">
+            <img 
+                src={AccessDenied} 
+                alt="puppy eating paper" 
+                className="w-60"
+            />
+            <h1 className="uppercase font-bebas text-5xl text-center my-6">access denied!</h1>
+
+            <div className="font-light text-sm text-center">
+                <p className="my-1">We're sorry but you do not have the required permission this page.</p>
+                <p className="my-1">Please contact the site owner.</p>
+            </div>
+
+            <div className="flex justify-center-safe mt-9">
+                <button className="bg-amber-500 px-10 py-3 rounded-3xl font-bold tracking-wide">Go Back</button>
+            </div>
+
+        </section> 
         </>
     )
 }
