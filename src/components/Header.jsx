8bcb9778a0501eeecdc77ddfbc06d2c131d60348
gabify <ivangabriel.talaoc@g.batstate-u.edu.ph>
2025-12-03 17:00:13 +0800

    Finished responsive log in ui

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,9 +6,9 @@ const Header = () =>{
     return(
         <>
             <header className="shadow-lg shadow-gray-500/5 font-Quicksand">
-                <section className="grid grid-cols-12 items-center-safe pb-2 px-2">
-                    <h1 className="col-span-5 md:col-span-2 lg:order-2 md:place-self-center-safe text-2xl font-bold font-bebas tracking-wide cursor-pointer max-lg:pt-2">Block Market</h1>
-                    <form className="hidden sm:flex items-center-safe col-start-6 md:col-start-4 col-span-3 lg:order-3 lg:col-start-9 my-2">
+                <section className="grid grid-cols-12 items-center-safe px-2">
+                    <h1 className="col-span-5 md:col-span-2 lg:order-1 md:place-self-center-safe text-2xl font-bold font-bebas tracking-wide cursor-pointer max-lg:pt-2">Block Market</h1>
+                    <form className="hidden sm:flex items-center-safe col-start-6 md:col-start-4 col-span-3 max-lg:col-span-4 lg:order-3 lg:col-start-9 my-2">
                         <input 
                             type="search" 
                             id="search-query" 
@@ -38,9 +38,9 @@ const Header = () =>{
                         </svg>
                     </div>
 
-                    <nav className={`max-md:col-span-12 ${isShown ? 'block' : 'hidden'} md:flex gap-0 col-start-8 lg:col-span-5 lg:order-1`}>
+                    <nav className={`max-md:col-span-12 ${isShown ? 'block' : 'hidden'} md:flex gap-0 md:col-start-9 lg:col-start-3 lg:col-span-3 lg:order-2 md:place-self-center-safe`}>
                         <a href="" className="flex items-center-safe text-sm py-2 px-2 -ms-1.5 text-gray-500 max-md:hover:text-gray-800 max-md:hover:bg-gray-50 max-md:hover:border-l-4 max-md:hover:border-amber-500 md:hover:border-t-4 md:hover:border-amber-500">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 md:hidden">
                                 <path d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z" />
                                 <path d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z" />
                             </svg>
@@ -48,21 +48,21 @@ const Header = () =>{
                         </a>
 
                         <a href="" className="flex items-center-safe text-sm py-2 px-2 -ms-1.5 text-gray-500 max-md:hover:text-gray-800 max-md:hover:bg-gray-50 max-md:hover:border-l-4 max-md:hover:border-amber-500 md:hover:border-t-4 md:hover:border-amber-500">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 md:hidden">
                                 <path fillRule="evenodd" d="M7.5 6v.75H5.513c-.96 0-1.764.724-1.865 1.679l-1.263 12A1.875 1.875 0 0 0 4.25 22.5h15.5a1.875 1.875 0 0 0 1.865-2.071l-1.263-12a1.875 1.875 0 0 0-1.865-1.679H16.5V6a4.5 4.5 0 1 0-9 0ZM12 3a3 3 0 0 0-3 3v.75h6V6a3 3 0 0 0-3-3Zm-3 8.25a3 3 0 1 0 6 0v-.75a.75.75 0 0 1 1.5 0v.75a4.5 4.5 0 1 1-9 0v-.75a.75.75 0 0 1 1.5 0v.75Z" clipRule="evenodd" />
                             </svg>
                             <span className="ms-1 font-light">Products</span>
                         </a>
 
                         <a href="" className="flex items-center-safe text-sm py-2 px-2 -ms-1.5 text-gray-500 max-md:hover:text-gray-800 max-md:hover:bg-gray-50 max-md:hover:border-l-4 max-md:hover:border-amber-500 md:hover:border-t-4 md:hover:border-amber-500">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 md:hidden">
                                 <path fillRule="evenodd" d="M3 6a3 3 0 0 1 3-3h2.25a3 3 0 0 1 3 3v2.25a3 3 0 0 1-3 3H6a3 3 0 0 1-3-3V6Zm9.75 0a3 3 0 0 1 3-3H18a3 3 0 0 1 3 3v2.25a3 3 0 0 1-3 3h-2.25a3 3 0 0 1-3-3V6ZM3 15.75a3 3 0 0 1 3-3h2.25a3 3 0 0 1 3 3V18a3 3 0 0 1-3 3H6a3 3 0 0 1-3-3v-2.25Zm9.75 0a3 3 0 0 1 3-3H18a3 3 0 0 1 3 3V18a3 3 0 0 1-3 3h-2.25a3 3 0 0 1-3-3v-2.25Z" clipRule="evenodd" />
                             </svg>
                             <span className="ms-1 font-light">Categories</span>
                         </a>
 
                         <a href="" className="inline-flex items-center-safe text-sm py-2 -ms-1.5 ps-2 pe-4 text-gray-500 hover:text-gray-50 hover:bg-amber-500 hover:rounded-md">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 md:hidden">
                                 <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clipRule="evenodd" />
                             </svg>
                             <span className="ms-1 font-light">Login</span>
--- src/pages/public/Login.jsx
+++ src/pages/public/Login.jsx
@@ -1,7 +1,79 @@
+import { useState } from "react";
+import Footer from "../../components/Footer.jsx";
+import Header from "../../components/Header.jsx";
+import Main from "../../components/Main.jsx";
+import Welcome from "../../assets/welcome_img.svg";
+
 const Login = () =>{
+    const [showPassword, setShowPassword] = useState(false);
+
     return(
         <>
-           
+            <Header />
+        
+            <Main>
+                <section className="p-3 my-20 mx-auto text-gray-800 lg:shadow-sm lg:rounded-2xl grid grid-cols-2 lg:w-6/12">
+                    <article className="hidden lg:grid grid-cols-1 px-2 items-baseline-last justify-center-safe bg-white rounded-xl col-span-1">
+                        <div className="font-bebas ">
+                            <div className="flex gap-0.5 justify-center-safe items-center-safe">
+                                <div className="text-xl">
+                                    <p className="text-amber-500">Discover</p>
+                                    <p>and</p>
+                                    <p className="text-amber-500">Manage</p>
+                                </div>
+                                <p className="text-9xl">Shop</p>
+                            </div>
+                            <p className="-mt-3 text-center text-lg tracking-wide bg-amber-500">Your store with ease</p>
+                        </div>
+                        <img src={Welcome} alt="Welcome Image" className="w-96"/>
+                    </article>
+                    <article className="max-lg:bg-white max-lg:shadow-sm max-lg:rounded-2xl pb-5 pt-10 px-5 min-w-64 sm:w-96 lg:w-72 place-self-center-safe col-span-2 lg:col-span-1">
+                        <div className="text-center mb-10">
+                            <h1 className="font-bebas text-3xl">Welcome back!</h1>
+                            <p className="font-light tracking-wide -mt-1">Please log in to your account</p>
+                        </div>
+                        <form action="" className="mb-10">
+                            <div className="grid grid-col-1 mb-3">
+                                <label htmlFor="email" className="text-sm mb-1.5 text-gray-600">Email</label>
+                                <input 
+                                    type="email" 
+                                    name="email" 
+                                    id="email" 
+                                    className="border-2 border-gray-400 rounded-md py-2 px-3"
+                                    placeholder="juan.delacruz@email.com"
+                                />
+                            </div>
+
+                            <div className="grid grid-col-1 mb-3">
+                                <label htmlFor="password" className="text-sm mb-1.5 text-gray-600">Password</label>
+                                <input 
+                                    type={showPassword ? 'text' : 'password'} 
+                                    name="password" 
+                                    id="password" 
+                                    className="border-2 border-gray-400 rounded-md py-2 px-3"
+                                />
+                            </div>
+
+                            <div className="flex justify-between mb-5">
+                               <div className="flex items-center-safe gap-0.5">
+                                 <input type="checkbox" name="showPassword" id="showPassword" onChange={()=> setShowPassword(!showPassword)}/>
+                                 <small className="text-xs">Show Password</small>
+                               </div>
+
+                               <a href="http://" className="text-xs text-amber-500 hover:underline underline-offset-2">Forgot Password?</a>
+                            </div>
+
+                            <div className="grid grid-cols-1">
+                                <button type="submit" className="bg-amber-500 hover:bg-amber-400 py-3 rounded-xl text-gray-50 font-black text-lg cursor-pointer">Log in</button>
+                            </div>
+                        </form>
+
+                        <small>No account yet? Register <a href="" className="text-amber-500 hover:underline">here</a></small>
+                    </article>
+                </section>
+            </Main>
+
+            <Footer />
         </>
     )
 }
