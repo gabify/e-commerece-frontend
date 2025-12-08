b8f064969286c3e4b50d73463f8f4d048cdbdfad
gabify <ivangabriel.talaoc@g.batstate-u.edu.ph>
2025-12-08 19:30:23 +0800

    Add route guard that disable admin to visit public pages

---
+++ src/routes/PublicRoutes.jsx
@@ -0,0 +1,13 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { use } from "react";
+import { AuthContext } from "../context/AuthContext";
+
+const PublicRoutes = () =>{
+    const {user} = use(AuthContext);
+
+    if(user.type === 'admin') return <Navigate to={'/admin'} replace/>
+
+    return <Outlet />
+}
+
+export default PublicRoutes;
