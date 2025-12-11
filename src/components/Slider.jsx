fd14f2da6f85891cee6f31210fa2fbb3ad54945d
gabify <ivangabriel.talaoc@g.batstate-u.edu.ph>
2025-12-11 14:54:34 +0800

    Fetch category and product from backend

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,6 +1,6 @@
 const Slider = ({children}) => {
     return(
-        <article className="flex overflow-x-auto slider">
+        <article className="flex overflow-x-auto slider md:grid md:grid-cols-4 lg:grid-cols-5">
             {children}
         </article>
     )
--- src/pages/public/Home.jsx
+++ src/pages/public/Home.jsx
@@ -4,6 +4,9 @@ import Main from "../../components/Main.jsx";
 import Carousel from "../../components/Carousel.jsx";
 import Slider from "../../components/Slider.jsx";
 
+import Hero4 from "../../assets/bg-carousel-4.jpg"
+import { use } from "react";
+
 const categories = [
     "BLocks & Building Materials",
     "Tools & Weapons",
@@ -50,21 +53,21 @@ const categories = [
 
     const carousel4 = () =>{
         return(
-            <div className='bg-hero-4 bg-cover bg-center w-screen snap-center shrink-0 px-4 flex flex-col'>
-                <div className="flex flex-col items-center justify-center mb-10 sm:mb-20 md:mb-30">
-                    <h2 className="text-gray-50 text-3xl sm:text-5xl md:text-6xl -mb-1 mt-4 font-bebas text-center tracking-wide">Fast Delivery</h2>
+            <div className='bg-hero-4 bg-cover bg-center w-screen snap-center shrink-0 px-4'>
+                <div className="max-lg:flex max-lg:flex-col items-center w-screen justify-center mb-10 sm:mb-20 md:mb-30 lg:mt-10 lg:mb-40">
+                    <h2 className="text-gray-50 text-3xl sm:text-5xl md:text-6xl -mb-1 mt-4 font-bebas tracking-wide">Fast Delivery</h2>
                     <h2 className="bg-amber-500/75 text-white px-3 inline-block text-shadow-md text-xl md:text-2xl font-bebas text-center">No Redstone Required.</h2>
                 </div>
-                <p className="text-xs sm:text-md md:text-lg md:tracking-tight font-light text-center text-gray-50">Your favorite Minecraft items delivered like magic.</p>
+                <p className="text-xs sm:text-md md:text-lg md:tracking-tight font-light max-lg:text-center text-gray-50">Your favorite Minecraft items delivered like magic.</p>
             </div>
         )
     }
 
     const carousel5 = () =>{
         return(
-            <div className='bg-hero-5 bg-cover bg-center w-screen snap-center shrink-0 px-4'>
-                <h2 className="text-gray-50 text-center text-xl sm:text-4xl md:text-5xl -mb-1.5 mt-4 font-bebas"><span className="text-amber-300">Build.</span> Trade. <span className="text-amber-300">Play.</span> Repeat.</h2>
-                <p className="text-xs sm:text-md md:text-lg md:tracking-tight font-light text-center text-gray-50 mb-10">Join thousands of crafters leveling up their world.</p>
+            <div className='bg-hero-5 bg-cover bg-center w-screen snap-center shrink-0 px-4 lg:flex lg:flex-col justify-end'>
+                <h2 className="text-gray-50 text-center text-xl sm:text-4xl md:text-5xl -mb-1.5 mt-4 font-bebas lg:text-left"><span className="text-amber-300">Build.</span> Trade. <span className="text-amber-300">Play.</span> Repeat.</h2>
+                <p className="text-xs sm:text-md md:text-lg md:tracking-tight lg:text-left font-light text-center text-gray-50 mb-10">Join thousands of crafters leveling up their world.</p>
             </div>
         )
     }
@@ -77,28 +80,70 @@ const slides = [
     carousel5
 ];
 
+const fetchCategories = async () =>{
+    const response = await fetch(`${import.meta.env.VITE_API}category/all`);
+    const data = await response.json();
+
+    return data.message;
+}
+
+const fetchProducts = async () =>{
+    const response = await fetch(`${import.meta.env.VITE_API}product/all`);
+    const data = await response.json();
+
+    return data.message[0];
+}
+
+const categoryPromise = fetchCategories();
+const productPromise = fetchProducts();
+
 const Home = () =>{
+    const categories = use(categoryPromise);
+    const products = use(productPromise);
+
     return(
         <>
         <Header />
             <Main>
                 <section className="lg:max-w-4xl xl:max-w-max mx-auto mb-10">
-                    <Carousel>
+                    <Carousel autoSlide={true}>
                         {slides.map((CarouselBanner, index) =>(
                             <CarouselBanner key={index}/>
                         ))}
                     </Carousel>
                 </section>
 
-                <section className="p-2">
+                <section className="p-2 text-gray-700 mb-10">
                     <h3 className="font-bold">Categories</h3>
                     <Slider>
                         {categories.map((c, i) => (
-                            <div className="p-3 hover:bg-gray-100" key={i}>
-                                <div className="w-18 h-18 bg-amber-400 text-center text-xs text-white">
-                                    <span className="mt-20">Image Unavailable</span>
-                                </div>
-                                <h5 className="text-sm">{c}</h5>
+                            <div className="p-3 w-24 md:w-56 rounded-md hover:bg-gray-100" key={i}>
+                               {c.thumbnail === '' ? (
+                                    <div className="mx-auto rounded-md w-20 md:w-48 h-20 md:h-48 bg-amber-400 text-center text-xs text-white">
+                                        <span className="mt-20">Image Unavailable</span>
+                                    </div>
+                               ) : (
+                                    <img src={`http://localhost:3000/category/thumbnails/${c.thumbnail}`} alt="" className="mx-auto rounded-md w-20 md:w-48 h-20 md:h-48"/>
+                               )}
+                                <h5 className="text-xs text-center truncate font-light">{c.category_name}</h5>
+                            </div>
+                        ))}
+                    </Slider>
+                </section>
+
+                <section className="p-3 text-gray-700 mb-10">
+                    <h3 className="font-bold">Featured Products</h3>
+                    <Slider>
+                        {products.map((p, i) =>(
+                            <div className="p-3 w-24 md:w-56 rounded-md hover:bg-gray-100" key={i}>
+                               {p.thumbnail === '' ? (
+                                    <div className="mx-auto rounded-md w-20 md:w-48 h-20 md:h-48 bg-amber-400 text-center text-xs text-white">
+                                        <span className="mt-20">Image Unavailable</span>
+                                    </div>
+                               ) : (
+                                    <img src={`http://localhost:3000/product/thumbnails/${p.thumbnail}`} alt="" className="mx-auto rounded-md w-20 md:w-48 h-20 md:h-48"/>
+                               )}
+                                <h5 className="text-xs text-center truncate font-light">{p.name}</h5>
                             </div>
                         ))}
                     </Slider>
